360f39cddc76d36a3609bccfa5f7fab78db24910
EmirhanSyl <61618968+EmirhanSyl@users.noreply.github.com>
2022-10-13 17:52:51 +0300

    Update

--- src/main/java/com/blackflower/thirdlab/ThirdLab.java
+++ src/main/java/com/blackflower/thirdlab/ThirdLab.java
@@ -10,6 +10,12 @@ public class ThirdLab {
         System.out.println("Rounded version of " + num + " is: " + RoundInt(num));
         
         System.out.println("The Biggest Number is: " + FindTheBiggest(15, 8, 7));
+        
+        System.out.println("85 is : " + FindDivisibles(85));
+        
+        System.out.println("Biggest Divisor is: " + BiggestDivisor(8, 6));        
+        System.out.println("Biggest Divisor is: " + FindBiggestDivisorBetterWay(6, 8));
+
     }
     
     public static String NumberValue(int num){
@@ -53,5 +59,70 @@ public class ThirdLab {
         }
         
         return maxNum;
+    }
+    
+    public static String FindDivisibles(int num){
+        
+        String result;
+        if (num % 10 == 0) {
+            result = "Divisible by both 5 and 2";
+        }
+        else if (num % 5 == 0) {
+            result = "Divisible by 5";
+        }
+        else if (num % 2 == 0) {
+            result = "Divisible by 2";
+        }
+        else{
+            result = "Divisible by neither 5 or 2";
+        }
+        
+        return result;
+    }
+    
+    public static void FindDiscountAmount(int price){
+        int discountAmount;
+        
+        if (price < 200) {
+            discountAmount = 10;
+        }
+        else if (price < 400) {
+            discountAmount = 15;
+        }
+        else{
+            discountAmount = 20;
+        }
+        
+        double result = (100 - discountAmount);
+    }
+    
+    public static int BiggestDivisor(int num1, int num2){
+
+        int biggestDivisor = 1;
+        
+        if (num1 % 4 == 0  || num2 % 4 == 0) {
+            biggestDivisor = 4;
+        }
+        else if (num1 % 3 == 0  || num2 % 3 == 0) {
+            biggestDivisor = 3;
+        }
+        else if (num1 % 2 == 0  || num2 % 2 == 0) {
+            biggestDivisor = 2;
+        }
+        
+        return biggestDivisor;
+    }
+    
+    public static int FindBiggestDivisorBetterWay(int num1, int num2){
+        
+        int maxDevisor = 1;
+        
+        for (int i = 1; i < 5; i++) {
+            if (num1 % i == 0 || num2 % i == 0) {
+                maxDevisor = i;
+            }
+        }
+        
+        return maxDevisor;
     }
 }
